c6498925fe992425abbbb597677598cedc3f31ba
Michael Hudson <mnkotagu@gmail.com>
2018-01-05 09:05:46 +0300

    Add Auto Complete JQuery Plugin

--- src/AppBundle/Controller/Consumer/MainController.php
+++ src/AppBundle/Controller/Consumer/MainController.php
@@ -2,9 +2,11 @@
 
 namespace AppBundle\Controller\Consumer;
 
+use AppBundle\Form\Consumer\DataLevelReportFormType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 
 class MainController extends Controller
@@ -16,7 +18,24 @@ class MainController extends Controller
     public function homepageAction()
     {
 
-        $data = [];
+        $datasets[0]=['name'=>'Average Number of Cases','icon'=>'bar-chart','path'=>'average-number-of-cases'];
+        $datasets[1]=['name'=>'Number of Staff','icon'=>'users','path'=>'number-of-staff'];
+        $datasets[2]=['name'=>'Building Conditions','icon'=>'building','path'=>'building-conditions'];
+        $datasets[3]=['name'=>'Land Ownership','icon'=>'map','path'=>'land-ownership'];
+        $datasets[4]=['name'=>'Environmental Conditions','icon'=>'tree','path'=>'environmental-conditions'];
+        $datasets[5]=['name'=>'Social Activities','icon'=>'map-pointer','path'=>'social-activities'];
+
+
+        $statistics[0] = ['label'=>'Court Levels','value'=>'5'];
+        $statistics[1] = ['label'=>'Courts','value'=>'1340'];
+        $statistics[2] = ['label'=>'Wards','value'=>'1400'];
+        $statistics[3] = ['label'=>'Districts','value'=>'200'];
+        $statistics[4] = ['label'=>'Zones','value'=>'10'];
+        $statistics[5] = ['label'=>'Regions','value'=>'20'];
+
+        $data['datasets'] = $datasets;
+        $data['statistics'] = $statistics;
+
 
         return $this->render(
             'public/homepage.html.twig',
@@ -24,12 +43,69 @@ class MainController extends Controller
         );
     }
 
-   
+
+    /**
+     * @Route("/dataset/{datasetName}", name="public_dataset_page")
+     * @param $datasetName
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function socialActivitiesDatasetAction($datasetName)
+    {
+
+        switch($datasetName)
+        {
+            case 'social-activities':$data=$this->getSocialActivitiesDataset();break;
+            case 'environmental-conditions':$data=$this->getEnvironmentalConditionsDataset();break;
+            case 'average-number-of-cases':$data=$this->getAverageNumberOfCasesDataset();break;
+            default: throw new NotFoundHttpException("Page Not Found");
+        }
 
 
+        return $this->render(
+            'public/dataset.page.html.twig',
+            $data
+        );
+    }
 
 
+    public function getSocialActivitiesDataset(){
+        $data['pageTitle'] = 'Social Activities';
 
+        $data['description'] = 'Lorem ipsum dolor sit amet, stet iudicabit ius ei, no case reque has. Mea habeo ignota meliore cu. Sint wisi hendrerit 
+        eu eum, populo invenire ad mea, vis cu sint atqui accumsan.
+         Nec ut solum iriure. Dolores expetenda liberavisse id vel.
+          Est indoctum moderatius liberavisse ei. 
+          Nec ei feugiat conclusionemque, wisi sanctus ea his.';
 
+        return $data;
+    }
+
+    public function getEnvironmentalConditionsDataset(){
+        $data['pageTitle'] = 'Environmental Conditions';
+
+        $data['description'] = 'Lorem ipsum dolor sit amet, stet iudicabit ius ei, no case reque has. Mea habeo ignota meliore cu. Sint wisi hendrerit 
+        eu eum, populo invenire ad mea, vis cu sint atqui accumsan.
+         Nec ut solum iriure. Dolores expetenda liberavisse id vel.
+          Est indoctum moderatius liberavisse ei. 
+          Nec ei feugiat conclusionemque, wisi sanctus ea his.';
+
+        return $data;
+    }
+
+    public function getAverageNumberOfCasesDataset(){
+        $data['pageTitle'] = 'Average Number of Cases';
+
+        $data['description'] = 'Lorem ipsum dolor sit amet, stet iudicabit ius ei, no case reque has. Mea habeo ignota meliore cu. Sint wisi hendrerit 
+        eu eum, populo invenire ad mea, vis cu sint atqui accumsan.
+         Nec ut solum iriure. Dolores expetenda liberavisse id vel.
+          Est indoctum moderatius liberavisse ei. 
+          Nec ei feugiat conclusionemque, wisi sanctus ea his.';
+
+        $form = $this->createForm(DataLevelReportFormType::class);
+
+        $data['form']=$form->createView();
+
+        return $data;
+    }
 
 }
---
+++ src/AppBundle/Form/Consumer/DataLevelReportFormType.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Form\Consumer;
+
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DataLevelReportFormType extends  AbstractType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('courtLevel', EntityType::class, [
+                'placeholder' => 'Choose court level',
+                'choice_label' => 'description',
+                'mapped'=>false,
+                'required'=>false,
+                'class' => 'AppBundle\Entity\Configuration\CourtLevel',
+                'query_builder' => function(EntityRepository  $er) {
+                    return $er->createQueryBuilder('r')
+                        ->orderBy('r.description', 'ASC');
+                }
+            ])
+            ->add('region', EntityType::class, [
+                'placeholder' => 'Choose a region',
+                'choice_label' => 'regionName',
+                'mapped'=>false,
+                'required'=>false,
+                'class' => 'AppBundle\Entity\Location\Region',
+                'query_builder' => function(EntityRepository  $er) {
+                    return $er->createQueryBuilder('r')
+                        ->orderBy('r.regionName', 'ASC');
+                }
+            ])
+            ->add('district', ChoiceType::class, array(
+                'placeholder' => 'Choose District',
+                'choices' => [],
+                'mapped' => false,
+                'required' => false
+            ))
+            ->add('ward', ChoiceType::class, array(
+                'placeholder' => 'Choose Ward',
+                'choices' => [],
+                'mapped' => false,
+                'required' => false
+            ));
+
+        $builder->get('region')->resetViewTransformers();
+        $builder->get('district')->resetViewTransformers();
+        $builder->get('ward')->resetViewTransformers();
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+        ]);
+    }
+
+
+}
