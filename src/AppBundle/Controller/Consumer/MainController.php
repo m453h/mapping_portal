59420fe53d40f9064d14ebac98d2d39814864af8
Michael Hudson <mnkotagu@gmail.com>
2018-11-18 15:02:41 +0300

    Add Error Pages

--- src/AppBundle/Controller/Consumer/MainController.php
+++ src/AppBundle/Controller/Consumer/MainController.php
@@ -19,6 +19,11 @@ class MainController extends Controller
      */
     public function homepageAction()
     {
+        return $this->render(
+            '::app.error.html.twig', [
+                'currentPosition'=>'about'
+            ]
+        );
 
         return $this->render(
             'public/homepage.html.twig',
